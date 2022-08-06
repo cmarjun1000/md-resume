0927eef92266a41762020d57e7bea4eaf8592b69
C M ARJUN <33662184+cmarjun1000@users.noreply.github.com>
2022-08-06 20:24:12 +0530

    Update App.tsx

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -2,14 +2,13 @@ import React, { useEffect } from 'react'
 import Container from '@material-ui/core/Container'
 import CssBaseline from '@material-ui/core/CssBaseline'
 import Grid from '@material-ui/core/Grid'
-import { makeStyles, Theme, createStyles, Box, Fab } from '@material-ui/core'
+import { makeStyles, Theme, createStyles, Box } from '@material-ui/core'
 import PersonalInfo from './PersonalInfo'
 import Projects from './Projects'
 import Experiences from './Experiences'
 import Education from './Education'
 import Skills from './Skills'
 import Languages from './Languages'
-import GetAppIcon from '@material-ui/icons/GetApp'
 import { useDispatch } from 'react-redux'
 import { getResume } from '../store'
 import BackdropLoader from './BackdropLoader'
@@ -42,8 +41,6 @@ const useStyles = makeStyles((theme: Theme) =>
 const App = () => {
   const classes = useStyles()
   const dispatch = useDispatch()
-
-  const fileUrl = `${window.location.origin}/resume.pdf`
 
   useEffect(() => {
     dispatch(getResume())
